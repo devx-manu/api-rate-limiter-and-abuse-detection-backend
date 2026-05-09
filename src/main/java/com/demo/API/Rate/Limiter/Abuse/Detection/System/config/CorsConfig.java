5536dace28b2c7bfadf72c0aa3555927e3f23059
Manu SH <devx.manu@gmail.com>
2026-05-09 12:32:36 +0530

    Fix CORS allowed origin URL format

--- src/main/java/com/demo/API/Rate/Limiter/Abuse/Detection/System/config/CorsConfig.java
+++ src/main/java/com/demo/API/Rate/Limiter/Abuse/Detection/System/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://api-rate-limiter-and-abuse-detectio.vercel.app/")
+                        .allowedOrigins("https://api-rate-limiter-and-abuse-detectio.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
